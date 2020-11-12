98f8aa52223f3f89b66172e853b48f6edc3be266
Sam Baker <Sam.Baker101@gmail.com>
2020-11-12 00:48:18 -0500

    Added More Waveforms
    Now has switchable waveforms for lsaw, rsaw and triangle

--- PWM3/PWM3.ino
+++ PWM3/PWM3.ino
@@ -7,6 +7,8 @@
 #define N 100
 
 int sine_table[100];
+int rsaw_table[100];
+int lsaw_table[100];
 int triangle_table[100];
 
 volatile int switch_state = LOW;
@@ -14,13 +16,17 @@ volatile int switch_state = LOW;
 int table = 0;
 /*
  * 0 - Sine
- * 1 - Triangle
+ * 1 - Right Saw
+ * 2 - Left Saw
+ * 3 - Triangle
  */
 
 
 int sample_time;
 
 void get_sine_table();
+void get_rsaw_table();
+void get_lsaw_table();
 void get_triangle_table();
 
 void setup() {
@@ -28,6 +34,8 @@ void setup() {
   attachInterrupt(digitalPinToInterrupt(SWITCH_PIN), switch_pin_ISR, FALLING);
   
   get_sine_table();
+  get_rsaw_table();
+  get_lsaw_table();
   get_triangle_table();
 }
 
@@ -42,8 +50,19 @@ void loop() {
         break;
 
       case 1:
+        analogWrite(PWM_PIN, rsaw_table[i]);
+        break;
+
+      case 2:
+        analogWrite(PWM_PIN, lsaw_table[i]);
+        break;
+
+      case 3:
         analogWrite(PWM_PIN, triangle_table[i]);
         break;
+
+
+      
       }
       
     delay(sample_time);
@@ -51,7 +70,7 @@ void loop() {
 }
 
 void switch_pin_ISR(){
-  table = (table + 1)%2;
+  table = (table + 1)%4;
   }
 
 void get_sine_table(){
@@ -60,8 +79,25 @@ void get_sine_table(){
   }
 }
 
+void get_rsaw_table(){
+  for (int i = 0; i < N; i++){
+    rsaw_table[i] = i*(255/N);
+  }
+}
+
+void get_lsaw_table(){
+  for (int i = 0; i < N; i++){
+    lsaw_table[i] = 255 - i*(255/N);
+  }
+}
+
 void get_triangle_table(){
   for (int i = 0; i < N; i++){
-    triangle_table[i] = i*(255/N);
+    if (i<(N/2)){
+      triangle_table[i] = i*(255/(N/2));
+      }
+    else{
+      triangle_table[i] = 255 - (i-N/2)*(255/(N/2));
+      }
   }
 }
